a37dac2f65d32509fce281a3b3500a6175a77d74
sveta <svetlana.fedoseeva.88@mail.ru>
2022-08-07 23:38:23 +0300

    первый коммит

---
+++ src/main/java/proskyspringskyprohw214/SpringSkyproHw214Application.java
@@ -0,0 +1,13 @@
+package proskyspringskyprohw214;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSkyproHw214Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSkyproHw214Application.class, args);
+	}
+
+}
